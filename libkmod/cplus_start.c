3019c5652379efe8e84991b6a856b0f2fc10fe40
William Kent <wjk@users.noreply.github.com>
2015-08-24 23:37:42 -0400

    Implement libkmodc++

--- libkmod/cplus_start.c
+++ libkmod/cplus_start.c
@@ -1,48 +1,58 @@
 /*
- * Copyright (c) 2000,2008-2012 Apple Inc. All rights reserved.
+ * Copyright (c) 2003-2007 Apple Inc. All Rights Reserved.
  *
- * @APPLE_OSREFERENCE_LICENSE_HEADER_START@
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. The rights granted to you under the License
- * may not be used to create, or enable the creation or redistribution of,
- * unlawful or unlicensed copies of an Apple operating system, or to
- * circumvent, violate, or enable the circumvention or violation of, any
- * terms of an Apple operating system software license agreement.
- * 
- * Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * @APPLE_LICENSE_HEADER_START@
+ *
+ * The contents of this file constitute Original Code as defined in and
+ * are subject to the Apple Public Source License Version 1.1 (the
+ * "License").  You may not use this file except in compliance with the
+ * License.  Please obtain a copy of the License at
+ * http://www.apple.com/publicsource and read it before using this file.
+ *
+ * This Original Code and all software distributed under the License are
+ * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
  * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_OSREFERENCE_LICENSE_HEADER_END@
- */
-/*
-    Subtle combination of files and libraries make up the C++ runtime system for kernel modules.  We are dependant on the KernelModule kmod.make and CreateKModInfo.perl scripts to be exactly instep with both this library module and the libkmod module as well.
-
-    If you do any maintenance on any of the following files make sure great care is taken to keep them in Sync.
-    KernelModule.bproj/kmod.make
-    KernelModule.bproj/CreateKModInfo.perl
-    KernelModule.bproj/kmodc++/pure.c
-    KernelModule.bproj/kmodc++/cplus_start.c
-    KernelModule.bproj/kmodc++/cplus_start.c
-    KernelModule.bproj/kmodc/c_start.c
-    KernelModule.bproj/kmodc/c_stop.c
-
-    The trick is that the linkline links all of the developers modules.  If any static constructors are used .constructors_used will be left as an undefined symbol.  This symbol is exported by the cplus_start.c routine which automatically brings in the appropriate C++ _start routine.  However the actual _start symbol is only required by the kmod_info structure that is created and initialized by the CreateKModInfo.perl script.  If no C++ was used the _start will be an undefined symbol that is finally satisfied by the c_start module in the kmod library.
-
-    The linkline must look like this.
-        *.o -lkmodc++ kmod_info.o -lkmod
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * @APPLE_LICENSE_HEADER_END@
  */
 
-/* The following preprocessor test must match exactly with the architectures
- * that define the CONFIG_STATIC_CPPINIT config option.
- */
+#include <libkern/OSKextLib.h>
+
+asm(".constructors_used = 0");
+asm(".private_extern .constructors_used");
+
+extern kmod_start_func_t *_realmain;
+extern kmod_info_t KMOD_INFO_NAME;
+
+// Functions defined in libkern/c++/OSRuntime.cpp
+extern kern_return_t OSRuntimeInitializeCPP(kmod_info_t *ki, void *data);
+extern kern_return_t OSRuntimeFinalizeCPP(kmod_info_t *ki, void *data);
+
+__attribute__((visibility("hidden"))) kern_return_t _start(kmod_info_t *ki, void *data) {
+    kern_return_t result = OSRuntimeInitializeCPP(ki, data);
+
+    if (result == KERN_SUCCESS && _realmain) {
+        result = (*_realmain)(ki, data);
+
+        // If _realmain failed, tear down C++.
+        if (result != KERN_SUCCESS) OSRuntimeFinalizeCPP(ki, data);
+    }
+
+    return result;
+}
+
+__attribute__((visibility("hidden"))) const char *OSKextGetCurrentIdentifier(void) {
+    return KMOD_INFO_NAME.name;
+}
+
+__attribute__((visibility("hidden"))) const char *OSKextGetCurrentVersionString(void) {
+    return KMOD_INFO_NAME.version;
+}
+
+__attribute__((visibility("hidden"))) OSKextLoadTag OSKextGetCurrentLoadTag(void) {
+    return (OSKextLoadTag) KMOD_INFO_NAME.id;
+}
