2a476302e9443968c09a2a75611c951f747750ca
William Kent <wjk@sunsol.me>
2016-11-25 20:33:05 -0500

    Typo fix

--- xcodesupport/copy-IOATA-headers.sh
+++ xcodesupport/copy-IOATA-headers.sh
@@ -13,6 +13,6 @@ PRIVATE_DIR="${XNU_INSTALL_ROOT}/System/Library/Frameworks/Kernel.framework/Vers
 mkdir -p "${PUBLIC_DIR}" "${PRIVATE_DIR}"
 cp "${SRCROOT}/IOATAFamily/IOATAFamilyPriv.h" "${PRIVATE_DIR}/IOATAFamilyPriv.h"
 
-for FILE in IOPCIATA.h IOATABusCommand.h IOATABusInfo.h IOATACommand.h IOATAController.h IOATADevConfig.h IOATADevice.h IOATATypes.h ATADeviceNub.h ATATimerEventSource.h IOATARegI186.h; do
+for FILE in IOPCIATA.h IOATABusCommand.h IOATABusInfo.h IOATACommand.h IOATAController.h IOATADevConfig.h IOATADevice.h IOATATypes.h ATADeviceNub.h ATATimerEventSource.h IOATARegI386.h; do
 	cp "${SRCROOT}/IOATAFamily/${FILE}" "${PUBLIC_DIR}/${FILE}"
 done
