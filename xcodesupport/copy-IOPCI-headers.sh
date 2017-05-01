694d6430fab2883868c4475d83ce0762101e6c16
William Kent <wjk@sunsol.me>
2017-04-30 20:27:05 -0400

    Add IOPCIHeaders target

---
+++ xcodesupport/copy-IOPCI-headers.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+INPUT_DIR=${SRCROOT}/IOPCI/Headers/pci
+
+function InstallHeaders {
+	local DIR=$1
+	shift
+
+	mkdir -p $DIR
+	cp -f "$@" $DIR
+}
+
+InstallHeaders "${XNU_INSTALL_ROOT}/System/Library/Frameworks/Kernel.framework/Versions/A/Headers/IOKit/pci" \
+	"${INPUT_DIR}/IOAGPDevice.h" "${INPUT_DIR}/IOPCIBridge.h" "${INPUT_DIR}/IOPCIDevice.h"
+InstallHeaders "${XNU_INSTALL_ROOT}/System/Library/Frameworks/Kernel.framework/Versions/A/PrivateHeaders/IOKit/pci" \
+	"${INPUT_DIR}/IOPCIConfigurator.h" "${INPUT_DIR}/IOPCIPrivate.h"
+InstallHeaders "${XNU_INSTALL_ROOT}/System/Library/Frameworks/IOKit.framework/Versions/A/PrivateHeaders/pci" \
+	"${INPUT_DIR}/IOPCIDevice.h" "${INPUT_DIR}/IOPCIPrivate.h"
